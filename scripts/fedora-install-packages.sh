087ec83730623a821cf593d76f98d0b462088d4f
Flaverton Rodrigues Rosa <sign@droposhado.org>
2022-08-04 23:54:56 -0300

    fedora - auto confirm packages install

--- scripts/fedora-install-packages.sh
+++ scripts/fedora-install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dnf install kernel-devel
+dnf install -y kernel-devel
 
 awk -F'#' '{print $1}' "${PWD}/.Dnffile" | awk 'NF > 0' | \
-	xargs sudo dnf install --setopt=install_weak_deps=False
+	xargs sudo dnf install --setopt=install_weak_deps=False -y
 
