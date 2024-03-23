7e60da2c26ade3cf9c92cf16eded34cadbcff9aa
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-03-23 10:15:33 -0300

    update debian sourcelist generation

--- scripts/debian-config-sourceslist.sh
+++ scripts/debian-config-sourceslist.sh
@@ -5,14 +5,14 @@
 # lsb_release -cs
 
 cat <<-EOF > /etc/apt/sources.list
-deb http://deb.debian.org/debian/ ${CODINAME} main contrib non-free
-deb-src http://deb.debian.org/debian/ ${CODINAME} main contrib non-free
+deb http://deb.debian.org/debian/ ${CODINAME} main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ ${CODINAME} main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free
-deb-src http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free
+deb http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free non-free-firmware
 
-deb http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free
-deb-src http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free
+deb http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free non-free-firmware
 EOF
 
 cat <<-EOF > /etc/apt/sources.list.d/deb.droposhado.org.list
