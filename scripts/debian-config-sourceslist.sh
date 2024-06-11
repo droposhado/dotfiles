76aa044aefdee11973dacc831337cd950b9d3c84
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-06-11 14:27:15 -0300

    removing old self hosted deb repo

--- scripts/debian-config-sourceslist.sh
+++ scripts/debian-config-sourceslist.sh
@@ -14,7 +14,3 @@ deb-src http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free 
 deb http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free non-free-firmware
 deb-src http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free non-free-firmware
 EOF
-
-cat <<-EOF > /etc/apt/sources.list.d/deb.droposhado.org.list
-deb [trusted=yes] https://deb.droposhado.org/ /
-EOF
--- scripts/debian-install-packages.sh
+++ scripts/debian-install-packages.sh
@@ -5,6 +5,3 @@ apt-get install -y "linux-headers-$(uname -r)" --no-install-recommends
 
 awk -F'#' '{print $1}' "${PWD}/.Aptfile" | awk 'NF > 0' | \
 	xargs sudo apt-get --no-install-recommends install -y
-
-awk -F'#' '{print $1}' "${PWD}/.Aptfile.selfpackaged" | awk 'NF > 0' | \
-	xargs sudo apt-get --no-install-recommends install -y
