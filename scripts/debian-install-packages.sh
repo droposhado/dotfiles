b632a0473068d7c9ce8c0c9dc8a55307969b24dc
Flaverton Rodrigues Rosa <sign@droposhado.org>
2025-10-21 13:36:32 -0300

    Split .Aptfile & Makefile, add resolved and timesyncd files

--- scripts/debian-install-packages.sh
+++ scripts/debian-install-packages.sh
@@ -5,3 +5,16 @@ apt-get install -y "linux-headers-$(uname -r)" --no-install-recommends
 
 awk -F'#' '{print $1}' "${PWD}/.Aptfile" | awk 'NF > 0' | \
 	xargs sudo apt-get --no-install-recommends install -y
+
+awk -F'#' '{print $1}' "${PWD}/.Aptfile.dev" | awk 'NF > 0' | \
+	xargs sudo apt-get --no-install-recommends install -y
+
+if [[ $AMD_DRIVER_ENABLED -eq 1 ]];then
+	awk -F'#' '{print $1}' "${PWD}/.Aptfile.amd" | awk 'NF > 0' | \
+	xargs sudo apt-get --no-install-recommends install -y
+fi
+
+if [[ $GUI_ENABLED -eq 1 ]];then
+	awk -F'#' '{print $1}' "${PWD}/.Aptfile.gui" | awk 'NF > 0' | \
+	xargs sudo apt-get --no-install-recommends install -y
+fi
