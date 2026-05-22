08057daf03196f10b4fcd62ea898b570c42d1b57
Flaverton Rodrigues Rosa <sign@droposhado.org>
2026-05-22 00:47:17 -0300

    remove sources.list update

--- .chezmoiscripts/linux/debian/run_once_before_00-packages_linux.sh
+++ .chezmoiscripts/linux/debian/run_once_before_00-packages_linux.sh
@@ -3,18 +3,6 @@
 set -e
 
 if command -v apt-get >/dev/null; then
-  CODINAME=$(lsb_release -cs)
-
-  echo "
-deb http://deb.debian.org/debian/ ${CODINAME} main contrib non-free non-free-firmware
-deb-src http://deb.debian.org/debian/ ${CODINAME} main contrib non-free non-free-firmware
-
-deb http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free non-free-firmware
-deb-src http://deb.debian.org/debian/ ${CODINAME}-updates main contrib non-free non-free-firmware
-
-deb http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free non-free-firmware
-deb-src http://security.debian.org/debian-security ${CODINAME}-security main contrib non-free non-free-firmware" | \
-  sudo tee /etc/apt/sources.list
 
   sudo apt-get update
   sudo apt-get install -y "linux-headers-$(uname -r)" --no-install-recommends
